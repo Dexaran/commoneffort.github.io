12b0b5ffe63a549f7fedea1517a65b71ea56e219
Dexaran <dexaran820@gmail.com>
2019-10-04 13:13:00 +0300

    Add files via upload

---
+++ build/precache-manifest.c81d0296db4449689a33e815b7faa3be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a460452c003b2930b7de2b90172705a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a289da9555d497a7048c",
+    "url": "/static/css/main.a9260565.chunk.css"
+  },
+  {
+    "revision": "a4e48a344ed826cac78d",
+    "url": "/static/js/2.1af375e3.chunk.js"
+  },
+  {
+    "revision": "a289da9555d497a7048c",
+    "url": "/static/js/main.3b3385ea.chunk.js"
+  },
+  {
+    "revision": "c9d05c593624fb4eabee",
+    "url": "/static/js/runtime~main.a0d78bcd.js"
+  }
+]);
